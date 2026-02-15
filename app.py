c0cf76ab61e251cbf1e3a5e0d6f0310d07077a1c
ayahashem24 <ayahashem589@gmail.com>
2026-02-15 15:12:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return """
-    <h1 style='text-align:center;margin-top:100px;'>Hello Ayoya 👋</h1>
-    """
+    return "<h1 style='text-align:center;margin-top:100px'>Hello Ayoya 👋</h1>"
+
